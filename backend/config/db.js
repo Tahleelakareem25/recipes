dcfd97512f7685f9080b5d28a9c3ec1017819ba0
Tahleela <tahleela432@gmail.com>
2026-03-03 23:06:35 +0530

    fix db path

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,16 @@
 // Node.js example
 const mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONGO_URI, {
+const connectDB=async()=>{
+  try{   
+    await mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-})
-.then(() => console.log("MongoDB connected"))
-.catch(err => console.log(err));
+});
+console.log('MongoDB connected');
+ } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
